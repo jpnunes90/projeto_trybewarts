fa7dee86d2289f884292006ff4b86de115ecdd07
jpnunes90 <jp-nunes@hotmail.com>
2022-06-07 19:49:46 -0300

    requisitos obrigatorios completados com exceção do 3

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function login(){
-    
+function login() {
+
+}
+
+function habilitar() {
+  if (document.getElementById('agreement').checked) {
+    document.getElementById('submit-btn').disabled = false;
+  } else {
+    document.getElementById('submit-btn').disabled = true;
+  }
 }
