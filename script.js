3606b0390ec6b67b8474af0bf70782013a494d4d
jpnunes90 <jp-nunes@hotmail.com>
2022-06-08 15:21:55 -0300

    100% dos requisitos obrigatorios e lint completos

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ function habilitar() {
     document.getElementById('submit-btn').disabled = true;
   }
 }
+
+const btn = document.querySelector('#login');
+btn.addEventListener('click', () => {
+  const email = document.querySelector('.header #email');
+  const password = document.querySelector('.header #password');
+  const valPassword = password.value;
+  const valEmail = email.value;
+  console.log(valEmail);
+  console.log(valPassword);
+  if (valEmail === 'tryber@teste.com' && valPassword === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+
 habilitar();
