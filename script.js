487fdc8a3b41233262772b59b656a1a28b63f21d
jpnunes90 <jp-nunes@hotmail.com>
2022-06-07 22:56:19 -0300

    lint ajustado. Removido codigo do arquivo script.js e realocado no index.html

--- script.js
+++
@@ -1,7 +0,0 @@
-function habilitar() {
-  if (document.getElementById('agreement').checked) {
-    document.getElementById('submit-btn').disabled = false;
-  } else {
-    document.getElementById('submit-btn').disabled = true;
-  }
-}
